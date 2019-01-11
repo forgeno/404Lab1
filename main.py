331b1508d973997fd7dd7d65fd0afa00a8c46249
Ivan Ma <ima@ualberta.ca>
2019-01-10 20:25:34 -0500

    Updated main.py to load raw file

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
 import requests
 
 print(requests.__version__)
-r = requests.get("")
-#print(r.text)
-#print(r.status_code)
+r = requests.get("https://raw.githubusercontent.com/forgeno/404Lab1/master/main.py")
+print(r.text)
+print(r.status_code)
